26932f1506484ea5492e5d649f735d31492379cf
alex <natalia42010822@mail.ru>
2024-03-12 07:35:07 +0300

    added a basic structure for a clean architecture

--- app/src/main/java/com/karpovich/shoppinglist/app/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/karpovich/shoppinglist/app/presentation/activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.karpovich.shoppinglist.app
+package com.karpovich.shoppinglist.app.presentation.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.karpovich.shoppinglist.app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
